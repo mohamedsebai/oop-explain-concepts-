4c832bc5b2c3d94e04d0358b4106556536ee658a
Mohamed sebai <mohamedseabeai@gmail.com>
2023-05-07 15:29:45 +0300

    Update apple.class.php

--- namespace/apple.class.php
+++ namespace/apple.class.php
@@ -1,5 +1,10 @@
 <?php
-
+// created by : mohamed sebai
+// github: https://github.com/mohamedsebai
+// facebook: www.facebook.com/mohamedseabeai
+// instagram: www.instagram.com/mohamedseabeai
+// linkedin: in/arrogantm
+// website: http://arrogantm.com
 // namspace
 
 namespace apple;
